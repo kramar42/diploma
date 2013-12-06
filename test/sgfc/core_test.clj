477ddf7ed002292a18b203d5ca2706cfe99bcd10
Alexey Kramarenko <kramar42@gmail.com>
2013-12-06 21:54:34 +0200

    Removed outdated code

--- test/sgfc/core_test.clj
+++ test/sgfc/core_test.clj
@@ -1,21 +1,5 @@
-(ns lab-0.core-test
+(ns sgfc.core-test
   (:require [clojure.test :refer :all]
-            [lab-0.core :refer :all ]))
-
-(deftest stack-test-push
-  (is ((push 1) '(1)))
-  (is ((push 2) '(2 1))))
+            [sgfc.core :refer :all ]))
 
 
-(deftest stack-test-pop
-  (is ((pop) 2))
-  (is ((pop) 1)))
-
-;(deftest stack-test-push-pop
-; (map '#(is ())))
-
-(deftest stack-all-test
-  (stack-test-push)
-  (stack-test-pop))
-
-(run-tests 'lab-0.core-test)
