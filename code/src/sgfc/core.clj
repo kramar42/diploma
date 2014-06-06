3ebbf7e343b3dce5fb40ffdf6bd346f276c4d427
Alexey Kramarenko <kramar42@gmail.com>
2014-06-06 20:09:21 +0300

    Added simple SGF lexer & parser.

--- code/src/sgfc/core.clj
+++ code/src/sgfc/core.clj
@@ -1,57 +1,131 @@
 (ns sgfc.core
   (:gen-class))
 
-(def file-name "resources/test.sgf")
-
-(def EBNF
-  {:Collection [:GameTree :repetition :GameTree]
-   :GameTree ["(" :Sequence :repetition :GameTree ")"]
-   :Sequence [:Node :repetition :Node]
-   :Node [";" :repetition :Property]
-   :Property [:PropIdent :PropValue
-              :repetition :PropValue]
-   :PropIdent [:UcLetter :repetition :UcLetter]
-   :PropValue ["[" :CValueType "]"]
-   :CValueType [:or [:ValueType :Compose]]
-   :ValueType [:or [:None :Number :Real :Double
-                     :Color :SimpleText :Text
-                     :Point :Move :Stone]]})
-
-(def Type
-  {
-   :None [""]
-   ;; "A" - "Z"
-   :UcLetter (map char (range (int \A) (+ (int \Z) 1)))
-   ;; "0" - "9"
-   :Digit (map #(. String valueOf %) (range 0 10))
-
-   :Number [:optional :or ["+" "-"] :Digit :repetition :Digit]
-   :Real [:Number :optional ["." :Digit :repetition :Digit]]
-
-   :Double [:or ["1" "2"]]
-   :Color [:or ["B" "W"]]
-
-   :SimpleText [:repetition :any-char]
-   :Text [:repetition :any-char]
-
-   :Point [:todo :implement]
-   :Move [:todo :implement]
-   :Stone [:todo :implement]
-
-   :Compose [:ValueType ":" :ValueType]})
+(def UcLetters (doall (map char (range (int \A) (+ (int \Z) 1)))))
 
 (defn lazy-read [file]
-  (let [rdr (clojure.java.io/reader file)]
+  (let [rdr (clojure.java.io/reader file)
+        is-closed? (atom false)]
     (fn next-char []
-      (lazy-seq
+      (if @is-closed?
+        nil
         (let [chr (.read rdr)]
           (if (= -1 chr)
-            (do (.close rdr) nil)     
-            (cons (char chr) (next-char))))))))
+            (do (.close rdr)
+                (swap! is-closed? complement)
+                nil)
+            (char chr)))))))
 
-(def ^:dynamic *lazy-read-fn*
-  (lazy-read file-name))
+(defn read-char [lazy-read-fn stack]
+  (if-let [result (first @stack)]
+    (do
+      (swap! stack rest)
+      result)
+    (lazy-read-fn)))
+
+(defn unread-char [chr stack]
+  (swap! stack (partial cons chr))
+  chr)
+
+(defn lexer [input-chars stack result]
+  (letfn [(process-chars [input-chars stack result]
+            (if-let [next-char (read-char input-chars stack)]
+              (condp #(some #{%2} %1) next-char
+                [\(] :treestart
+                [\)] :treeend
+                [\;] :nodestart
+                UcLetters (list :propident
+                                (apply str (cons next-char
+                                                 (process-prop-ident input-chars stack))))
+                [\[] (list :propvalue
+                           (apply str (process-prop-value input-chars stack)))
+                [\]] :propvalueend
+                (recur input-chars stack result))
+              nil))
+
+          (process-prop-ident [input-chars stack]
+            (if-let [next-char (read-char input-chars stack)]
+              (if (some #{next-char} UcLetters)
+                (cons next-char (process-prop-ident input-chars stack))
+                (do (unread-char next-char stack) nil))))
+
+          (process-prop-value [input-chars stack]
+            ; teach to skip \] in values
+            (if-let [next-char (read-char input-chars stack)]
+              (if (= next-char \])
+                (do (unread-char next-char stack) nil)
+                (cons next-char (process-prop-value input-chars stack)))))]
+
+    (if-let [next-token (process-chars input-chars stack result)]
+      (recur input-chars stack (cons next-token result))
+      result)))
+
+
+(defn parser [lexems]
+  (letfn [(process-file [lexems tree]
+            ;(println "process-files: " (first lexems))
+            (let [lexem (first lexems)
+                  lexems (rest lexems)]
+              (condp = lexem
+                :treestart (let [[lexems subtree] (process-tree lexems nil)
+                                 subtree (reverse subtree)]
+                             (recur lexems (cons subtree tree)))
+                :nodestart (let [[lexems subtree] (process-tree (cons lexem lexems) nil)
+                                 subtree (reverse subtree)]
+                             (recur lexems (cons subtree tree)))
+                :treeend (recur lexems tree)
+                (reverse tree))))
+
+          (process-tree [lexems tree]
+            ;(println "process-tree: " (first lexems))
+            (let [lexem (first lexems)
+                  lexems (rest lexems)]
+              (condp = lexem
+                :nodestart (let [[lexems subtree] (process-nodes lexems nil)
+                                 subtree (reverse subtree)]
+                             (recur lexems (cons subtree tree)))
+                (list (cons lexem lexems) tree))))
+
+          (process-nodes [lexems tree]
+            ;(println "process-nodes: " (first lexems))
+            (if (and
+                  (instance? clojure.lang.PersistentList (first lexems))
+                  (= (ffirst lexems) :propident))
+              (let [propident (second (first lexems))
+                    [lexems subtree] (process-values (rest lexems) nil)
+                    subtree (reverse subtree)]
+                (recur lexems (cons (cons propident subtree) tree)))
+              (list lexems tree)))
+
+          (process-values [lexems tree]
+            ;(println "process-values: " (first lexems))
+            (if (and
+                  (instance? clojure.lang.PersistentList (first lexems))
+                  (= (ffirst lexems) :propvalue)
+                  (= (second lexems) :propvalueend))
+              (recur (nnext lexems) (cons (second (first lexems)) tree))
+              (list lexems tree)))]
+
+    (process-file lexems nil)))
+
+(defn process-file [file-name]
+  (let [lazy-read-fn (lazy-read file-name)
+        stack (atom nil)
+        lexems (reverse (lexer lazy-read-fn stack nil))
+        tree   (parser lexems)]
+    tree))
+
+(defn simplify [tree]
+  (if (instance? java.lang.String (first tree))
+    ;; process property ident/value pair
+    (let [color (first tree)
+          move (second tree)]
+      (if (or
+            (= color "W")
+            (= color "B"))
+        move))
+    ;; process list
+    (filter (complement nil?) (apply list (map simplify tree)))))
 
 (defn -main [& args]
-
-  )
+  (process-file (first args)))
